7d9ebfae680c45601a447c269daf8d620ffa37eb
SuperJu5tin <superjuslib@gmail.com>
2022-10-03 13:40:32 -0600

    yea

--- Snake_Rust/src/main.rs
+++ Snake_Rust/src/main.rs
@@ -1,67 +1,13 @@
-use piston_window::WindowSettings;
-use piston_window::PistonWindow;
+extern crate piston_window;
 
-struct ColoredRect {
-	pub color: [f32; 4],
-	pub position: [f64; 4],
-	velocity: [f64; 2]
-}
-
-impl ColoredRect {
-	fn new() -> Self {
-			ColoredRect {
-					color: [1.0, 0.5, 0.25, 1.0],
-					position: [0.0, 0.0, 100.0, 100.0],
-					velocity: [0.3, 0.3]
-			}
-	}
-}
-
-fn update_color(color: f32)->f32 {
-	if color <= 0.0 {
-			1.0
-	} else {
-			color - 0.001
-	}
-}
-
-fn update(&mut self, size: (f64, f64)) {
-	self.color[0] = Self::update_color(self.color[0]);
-	self.color[1] = Self::update_color(self.color[1]);
-	self.color[2] = Self::update_color(self.color[2]);
-	
-	// Collision check X
-	// Move X
-
-	// Collision check Y
-	// Move Y
-}
-
-fn update(&mut self, size: (f64, f64)) {
-	self.color[0] = Self::update_color(self.color[0]);
-	self.color[1] = Self::update_color(self.color[1]);
-	self.color[2] = Self::update_color(self.color[2]);
-	// X updates
-	if self.position[0] + self.position[2] >= size.0 ||
-			self.position[0] < 0.0 {
-			self.velocity[0] = -self.velocity[0];
-	}
-	self.position[0] += self.velocity[0];
-
-	// Y updates
-	if self.position[1] + self.position[3] >= size.1 || 
-			self.position[1] < 0.0 {
-			self.velocity[1] = -self.velocity[1];
-	} 
-	self.position[1] += self.velocity[1];
-}
+use piston_window::*;
 
 fn main() {
-	let mut rect = ColoredRect::new();
-	let mut window: PistonWindow =
-			WindowSettings::new("Hello Piston!", [640, 480])
-			.exit_on_esc(true)
-			.vsync(true)
-			.build().unwrap();
-	// Do things that have to be done every frame
-} // Rust's automagical resource management means we're done!
+  let mut window: PistonWindow = WindowSettings::new("Hello Piston!", [640, 480]).exit_on_esc(true).build().unwrap();
+  while let Some(e) = window.next() {
+      window.draw_2d(&e, |c, g, _device| {
+          clear([1.0; 4], g);
+          rectangle([0.0, 1.0, 0.0, 1.0], [0.0, 0.0, 50.0, 50.0], c.transform, g);
+    });
+  }
+}
