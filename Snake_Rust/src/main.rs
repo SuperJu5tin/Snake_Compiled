f5caabbf2dbaaf5efd138fa58e103cbde5b1baa4
SuperJu5tin <superjuslib@gmail.com>
2022-10-25 13:47:52 -0600

    I have squares now

--- Snake_Rust/src/main.rs
+++ Snake_Rust/src/main.rs
@@ -3,49 +3,68 @@ extern crate graphics;
 extern crate opengl_graphics;
 extern crate piston;
 
-use glutin_window::GlutinWindow as Window;
-use opengl_graphics::{GlGraphics, OpenGL};
-use piston::event_loop::{EventSettings, Events};
-use piston::input::{RenderArgs, RenderEvent, UpdateArgs, UpdateEvent};
 use piston::window::WindowSettings;
-
-enum Direction {
-  Right, Left, Up, Down
-}
+use piston::event_loop::*;
+use piston::input::*;
+use glutin_window::GlutinWindow;
+use opengl_graphics::{GlGraphics, OpenGL};
 
 struct App {
   gl: GlGraphics,
-  rotation: f64,
-  pos_x; i32,
-  pos_y: i32,
+  snake: Snake,
+  food: Food,
 }
 
 impl App {
   fn render(&mut self, args: &RenderArgs) {
-    use graphics::*;
-    const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
-    const RED: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
     const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 0.0];
 
-    let square = graphics::rectangle::square(self.pos_x as f64, self.pos_y as f64, 20_f64;
-
-    self.gl.draw(args.viewport(), |c, gl| {
-      clear(BLACK, gl);
-      let transform = c
-        .transform
-        .trans(self.pos_x, self.pos_y)
-        .rot_rad(rotation)
-        .trans(-25.0, -25.0);
-      rectangle(GREEN, square, transform, gl);
+    self.gl.draw(args.viewport(), |_c, gl| {
+      graphics::clear(BLACK, gl)
     });
 
-    self.snake.render(&mut self.gl, arg);
+    self.snake.render(&mut self.gl, args);
+    self.food.render(&mut self.gl, args);
   }
-  fn update(&mut self, args: &UpdateArgs) {
-    match self.dir {
-      self.pos_x += 1;
-      self.pos_y += 1;
-    }
+}
+
+struct Snake {
+  pos_x: i32,
+  pos_y: i32,
+}
+
+impl Snake {
+  fn render(&self, gl: &mut GlGraphics, args: &RenderArgs) {
+
+    let red: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+
+    let square = graphics::rectangle::square(self.pos_x as f64 + 1.0, self.pos_y as f64 + 1.0, 18_f64);
+
+    gl.draw(args.viewport(), |c, gl| {
+      let transform = c.transform;
+
+      graphics::rectangle(red, square, transform, gl)
+    })
+  }
+}
+
+struct Food {
+  pos_x: i32,
+  pos_y: i32,
+}
+
+impl Food {
+  fn render(&self, gl: &mut GlGraphics, args: &RenderArgs) {
+
+    let green: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
+
+    let square = graphics::rectangle::square(self.pos_x as f64, self.pos_y as f64, 20_f64);
+
+    gl.draw(args.viewport(), |c, gl| {
+      let transform = c.transform;
+
+      graphics::rectangle(green, square, transform, gl)
+    })
   }
 }
 
@@ -56,23 +75,17 @@ fn main() {
     .exit_on_esc(true)
     .build()
     .unwrap();
-  
+
   let mut app = App {
     gl: GlGraphics::new(opengl),
-    snake: Snake { pos_x: 0, pos_y: 0, 10 }
-    rotation: 0.0,
-    pos_x: args.window_size[0] / 2.0,
-    pos_y: args.window_size[1] / 2.0
+    snake: Snake { pos_x: 50, pos_y: 100 },
+    food: Food { pos_x: 140, pos_y: 100 },
   };
 
-  let mut events = Events::new(EventSettings::new()).ups(8);
+  let mut events = Events::new(EventSettings::new());
   while let Some(e) = events.next(&mut window) {
-    if let Some(args) = e.render_args() {
-      app.render(&args);
-    }
-
-    if let Some(args) = e.update_args() {
-      app.update(&args);
+    if let Some(r) = e.render_args() {
+      app.render(&r);
     }
   }
 }
