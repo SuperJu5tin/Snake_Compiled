815bae2ef3723d64c3ebcf074f9496c66406075d
SuperJu5tin <105902628+SuperJu5tin@users.noreply.github.com>
2022-11-14 16:04:36 -0700

    added food rand and started end game

--- Snake_Rust/src/main.rs
+++ Snake_Rust/src/main.rs
@@ -2,29 +2,45 @@ extern crate glutin_window;
 extern crate graphics;
 extern crate opengl_graphics;
 extern crate piston;
+extern crate rand;
 
+use std::{thread, time};
 use piston::window::WindowSettings;
 use piston::event_loop::*;
 use piston::input::*;
 use glutin_window::GlutinWindow;
 use opengl_graphics::{GlGraphics, OpenGL};
+use rand::Rng;
 
 
 struct App {
   gl: GlGraphics,
   snake: Snake,
   food: Food,
+  score: i32,
+  is_playing: bool,
 }
 
 impl App {
-  fn add_snake(&mut self) {
-    self.snake.pos.insert(0, vec![self.pos[0][0] + self.direction[0][0], self.pos[0][1] + self.direction[0][1]])
-    let isfood = false;
-    for i in &self.pos {
-      let i: Vec<i32> = i.to_vec();
-      if (i == self.food) {
 
+  fn add_snake(&mut self) {
+    self.snake.pos.insert(0, vec![self.snake.pos[0][0] + self.snake.direction[0][0], self.snake.pos[0][1] + self.snake.direction[0][1]]);
+    let mut is_food = false;
+    for i in &self.snake.pos {
+      let i: Vec<i32> = i.to_vec();
+      if 0 > i[0] || i[0] > 480 || 0 > i[1] || i[1] > 360 {
+        self.is_playing = false;
+        println!("thanks for playing")
       }
+      if i == self.food.pos {
+        is_food = true
+      }
+    }
+    if is_food {
+      self.food.randomize_food();
+      self.score+=1
+    } else {
+      self.snake.pos.remove(self.snake.pos.len() - 1);
     }
   }
   fn render(&mut self, args: &RenderArgs) {
@@ -34,10 +50,12 @@ impl App {
       graphics::clear(BLACK, gl)
     });
 
-    self.snake.add_snake();
+    self.add_snake();
 
     self.snake.render(&mut self.gl, args);
     self.food.render(&mut self.gl, args);
+
+    thread::sleep(time::Duration::from_millis(100));
   }
 }
 
@@ -47,14 +65,13 @@ struct Snake {
 }
 
 impl Snake {
-  
   fn pressed(&mut self, btn: &Button) {
     match btn {
-      &Button::Keyboard(Key::Up) => println!("Up!"),
-      &Button::Keyboard(Key::Down) => println!("Down!"),
-      &Button::Keyboard(Key::Left) => println!("Left!"),
-      &Button::Keyboard(Key::Right) => println!("Right!"),
-      _ => self.direction.insert(0, vec![10, 20])
+      &Button::Keyboard(Key::Up) => self.direction.insert(0, vec![0, -10]),
+      &Button::Keyboard(Key::Down) => self.direction.insert(0, vec![0, 10]),
+      &Button::Keyboard(Key::Left) => self.direction.insert(0, vec![-10, 0]),
+      &Button::Keyboard(Key::Right) => self.direction.insert(0, vec![10, 0]),
+      _ => ()
     }
   }
   fn render(&mut self, gl: &mut GlGraphics, args: &RenderArgs) {
@@ -70,12 +87,16 @@ impl Snake {
         graphics::rectangle(red, square, transform, gl)
       })
     }
-  } 
+  }
+  // fn check_snake() {
+  //   if (i[0] > 480 || i[0] < 0 || i[1] > 360 || i[1] < 0) {
+  //     println!("you lost")
+  //   } 
+  // }
 }
 
 struct Food {
-  pos_x: i32,
-  pos_y: i32,
+  pos: Vec<i32>,
 }
 
 impl Food {
@@ -83,13 +104,16 @@ impl Food {
 
     let green: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
 
-    let square = graphics::rectangle::square(self.pos_x as f64, self.pos_y as f64, 10_f64);
+    let square = graphics::rectangle::square(self.pos[0] as f64, self.pos[1] as f64, 10_f64);
 
     gl.draw(args.viewport(), |c, gl| {
       let transform = c.transform;
 
       graphics::rectangle(green, square, transform, gl)
     })
+  }
+  fn randomize_food(&mut self) {
+    self.pos = vec![rand::thread_rng().gen_range(0, 48) * 10, rand::thread_rng().gen_range(0, 36) * 10]
   }
 }
 
@@ -107,10 +131,12 @@ fn main() {
       pos: vec![vec![160, 180]],
       direction: vec![vec![10, 0]],
     },
-    food: Food { pos_x: 320, pos_y: 180 },
+    food: Food { pos: vec![320, 180] },
+    score: 0,
+    is_playing: true,
   };
 
-  let mut events = Events::new(EventSettings::new()).ups(10);
+  let mut events = Events::new(EventSettings::new());
   while let Some(e) = events.next(&mut window) {
     if let Some(r) = e.render_args() {
       app.render(&r);
